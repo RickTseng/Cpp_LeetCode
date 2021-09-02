ca6f8ff1e8b77a2ba7f93715ea79aee3553ca71c
Rick Tseng <ghost108@hotmail.com>
2021-09-02 15:41:48 +0800

    V1.0.0 210902

---
+++ 0169_MajorityElement.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<stdio.h>
+using namespace std;
+class Solution{
+    public:
+        int majorityElement(vector<int>& nums) {
+        
+        }
+};
+
+
+
+int main(){
+    Solution solution;
+    vector<int> nums = {2,2,3,2};
+    int x = solution.majorityElement(nums);
+}
