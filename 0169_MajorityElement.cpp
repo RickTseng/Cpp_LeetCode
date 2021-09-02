a866dcde23b56905bf2c5ac4ece758224f984820
Rick Tseng <ghost108@hotmail.com>
2021-09-02 16:27:35 +0800

    V1.0.1 210902(accept)

--- 0169_MajorityElement.cpp
+++ 0169_MajorityElement.cpp
@@ -1,10 +1,32 @@
 #include<vector>
 #include<stdio.h>
+#include<map>
 using namespace std;
 class Solution{
     public:
         int majorityElement(vector<int>& nums) {
-        
+            if(nums.size()==1){
+                return nums[0];
+            }
+            map<int,int> _map;
+            int _max = 0;
+            int _maxValue = 0;
+            for(int i =0;i<nums.size();i++){
+                if(_map.find(nums[i])==_map.end()){
+                    _map[nums[i]] = 1;
+                }
+                else{
+                    _map[nums[i]] += 1;
+                    if(_map[nums[i]]>_max){
+                        _max = _map[nums[i]];
+                        _maxValue = nums[i];
+                    }
+                    
+                }
+                
+            }
+            return _maxValue;
+
         }
 };
 
@@ -12,6 +34,7 @@ class Solution{
 
 int main(){
     Solution solution;
-    vector<int> nums = {2,2,3,2};
+    vector<int> nums(INT16_MAX,1);
+    
     int x = solution.majorityElement(nums);
 }
