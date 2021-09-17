2f05a3fbc2581fc7c9b128c301204c82fac510f5
Rick Tseng <ghost108@hotmail.com>
2021-09-17 17:18:35 +0800

    V1.0.0 210917(accept)

---
+++ 0198_House_Robber.cpp
@@ -0,0 +1,64 @@
+#include<vector>
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+
+
+class Solution_org{
+public:
+    int rob(vector<int>& nums) {
+        int _max = 0;
+        
+        for(int i = 0;i<nums.size();i++){
+            vector<int> sum(nums.size(),0);
+            dfs(nums,sum,i,0,_max);
+        }
+        return _max;
+    }
+    void dfs(vector<int>& nums,vector<int>& sum ,int index, int count, int &_max){
+        count+=nums[index];
+        _max = max(_max,count);
+        sum[index] = _max;
+        int next = 2;
+        while (index+next<nums.size())
+        {
+            
+            dfs(nums,sum,index+next,count,_max);
+            next++;
+        }
+        
+    }
+};
+//Web solution
+class Solution{
+    public:
+    int rob(vector<int>& nums) {
+        int _max = 0;
+        vector<int> res(nums.size(),-1);
+        return dfs(nums,nums.size()-1,res);
+    }
+    int dfs(vector<int>& nums,int index,vector<int>& res){
+        if(index < 0){
+             return 0;
+        }      
+        if(res[index] >= 0){
+            return res[index];
+        }
+        res[index]=max(dfs(nums,index-1,res),dfs(nums,index-2,res)+nums[index]);
+        return res[index];
+        
+    }
+};
+int main(){
+    Solution sol;
+
+    vector<int> nums = {2,7,9,3,1,1,2,1,5,1,5,6,4,8,7,4,5,1,3,0,1,2,1,5,4,8,4,5,1,2,3,4,8,7};
+    //vector<int> nums(100,1);
+    int ans = sol.rob(nums);
+
+
+
+    //return 0;
+}
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for House Robber.
+//Memory Usage: 7.9 MB, less than 5.69% of C++ online submissions for House Robber.
